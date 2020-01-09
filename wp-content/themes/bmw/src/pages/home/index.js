3a4a51cbe29bbf1428d2116b91bae5b7d61fb295
htrigoso <trigosoparedes@gmail.com>
2020-01-09 16:43:57 -0500

    udpate tabs page home

--- wp-content/themes/bmw/src/pages/home/index.js
+++ wp-content/themes/bmw/src/pages/home/index.js
@@ -2,9 +2,35 @@ import "./index.scss";
 
 import "slick-carousel";
 
+$(".sliderHome .slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false
+});
+
 $(".sectionFeaturedProducts .products").slick({
   slidesToShow: 3,
   slidesToScroll: 3,
   dots: true,
   arrows: false
+});
+
+window.onload = function() {
+  const actWidth = $(".tabs ul")
+    .find(".active")
+    .width();
+  console.log("width", actWidth);
+  const actPosition = $(".tabs ul .active").position();
+  $(".tabs .line").css({ left: +actPosition.left, width: actWidth });
+};
+
+$(".tabs ul li a").click(function() {
+  var position = $(this)
+    .parent()
+    .position();
+  var width = $(this)
+    .parent()
+    .width();
+  $(".tabs .line").css({ left: +position.left, width: width });
 });
--- wp-content/themes/bmw/templates/template-home.php
+++ wp-content/themes/bmw/templates/template-home.php
@@ -14,64 +14,86 @@
 ?>
 
 <section class="sliderHome">
-    <div class="swiper-container" data-swiper-id="swiper-home">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide" style="background-image: url(<?= get_template_directory_uri() ?>/assets/1.png)">
-            </div>
-            <div class="swiper-slide" style="background-image: url(<?= get_template_directory_uri() ?>/assets/1.png)">
-            </div>
-            <div class="swiper-slide" style="background-image: url(<?= get_template_directory_uri() ?>/assets/1.png)">
-            </div>
-        </div>
-    </div>
-    <div class="swiper-pagination"></div>
+    <ul class="slider">
+        <?php
+        for ($i = 1; $i <= 4; $i++) :
+        ?>
+            <li class="slider__item">
+                <img src="<?= get_template_directory_uri() ?>/assets/1.png" alt="">
+                <div class="x-container">
+                    <div class="x-caption">
+                        <h1 class="title">BMW LIFESTYLE <?= $i ?></h1>
+                        <p class="info">
+                            La vida esta llena de aventuras, las posibilidades son infinitas, la libertad y el estilo no conoce límites.
+                        </p>
+                        <a href="/" class="button button-primary">BMW Sale</a>
+                    </div>
+                </div>
+            </li>
+        <?php
+        endfor;
+        ?>
+
+    </ul>
 </section>
 
 <section class="sectionCategories">
-    <h1>Categorias</h1>
-    <?php
-    $args = array(
-        'taxonomy' => 'product_cat',
-        'hide_empty' => false,
-        'parent'   => 0
-    );
-    $product_cat = get_terms($args);
+    <div class="sectionCategories__content x-container">
+        <h3 class="title">Categorías</h3>
+        <?php
+        $args = array(
+            'taxonomy' => 'product_cat',
+            'hide_empty' => false,
+            'parent'   => 0
+        );
+        $product_cat = get_terms($args);
 
-    ?>
-    <ul>
-        <?php foreach ($product_cat as $parent_product_cat) : ?>
-            <li>
-                <a href="<?= get_term_link($parent_product_cat->term_id) ?>"><?= $parent_product_cat->name ?></a>
-                <?php
-                $child_args = array(
-                    'taxonomy' => 'product_cat',
-                    'hide_empty' => false,
-                    'parent'   => $parent_product_cat->term_id
-                );
-                $child_product_cats = get_terms($child_args);
-                ?>
-                <ul>
+        ?>
+        <div class="tabs">
+            <ul>
+                <?php foreach ($product_cat as $key => $parent_product_cat) : ?>
                     <?php
-                    foreach ($child_product_cats as $child_product_cat) :
-
-                        $thumbnail_id = get_woocommerce_term_meta($child_product_cat->term_id, 'thumbnail_id', true);
-
-                        // get the image URL
-                        $image = wp_get_attachment_url($thumbnail_id);
-
-
+                    if ($key === 0) :
                     ?>
-                        <li>
-                            <a href="<?= get_term_link($child_product_cat->term_id) ?>"><?= $child_product_cat->name ?></a>
-                            <img src="<?= $image ?>" width="50" alt="">
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
+                        <div class="line"></div>
+                    <?php endif; ?>
+                    <li class="<?= $key === 0 ? 'active' : '' ?>">
+                        <a href="javascript:void(0)"><?= $parent_product_cat->name ?></a>
+                    </li>
+                <? endforeach; ?>
+            </ul>
+        </div>
+        <!-- 
+        <ul>
+            <?php foreach ($product_cat as $parent_product_cat) : ?>
+                <li>
+                    <a href="<?= get_term_link($parent_product_cat->term_id) ?>"><?= $parent_product_cat->name ?></a>
+                    <?php
+                    $child_args = array(
+                        'taxonomy' => 'product_cat',
+                        'hide_empty' => false,
+                        'parent'   => $parent_product_cat->term_id
+                    );
+                    $child_product_cats = get_terms($child_args);
+                    ?>
+                    <ul>
+                        <?php
+                        foreach ($child_product_cats as $child_product_cat) :
+                            $thumbnail_id = get_woocommerce_term_meta($child_product_cat->term_id, 'thumbnail_id', true);
+                            $image = wp_get_attachment_url($thumbnail_id);
+                        ?>
+                            <li>
+                                <a href="<?= get_term_link($child_product_cat->term_id) ?>"><?= $child_product_cat->name ?></a>
+                                <img src="<?= $image ?>" width="50" alt="">
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
 
-            </li>
-        <?php endforeach; ?>
-    </ul>
-
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        !-->
+    </div>
 </section>
 
 

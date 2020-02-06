f0745fbe14b7b655dc8ca3b328a3801633c3bf12
htrigoso <trigosoparedes@gmail.com>
2020-02-06 15:46:08 -0500

    update thank

--- wp-content/themes/bmw/templates/template-thank-checkout.php
+++ wp-content/themes/bmw/templates/template-thank-checkout.php
@@ -11,7 +11,6 @@ get_header('header');
 set_query_var('class', 'is-active');
 get_template_part('partials/global/content', 'navbar');
 $order      = wc_get_order(136);
-
 $items = $order->get_items();
 ?>
 
@@ -33,51 +32,109 @@ $items = $order->get_items();
 		</div>
 	</div>
 	<div class="thankCheckout__content x-container">
-		<div class="order">
-			<h2>TU ORDEN HA SIDO RECIBIDA</h2>
-			<main>
-				<div class="item header">
-					<ul>
-						<li>Número de orden</li>
-						<li>Fecha</li>
-						<li>Total</li>
-						<li>Método</li>
-					</ul>
+		<?php if (!wp_is_mobile()) : ?>
+			<div class="desktop">
+				<div class="order">
+					<h2>TU ORDEN HA SIDO RECIBIDA</h2>
+					<table>
+						<thead>
+							<th>Número de orden</th>
+							<th>Fecha</th>
+							<th>Total</th>
+							<th>Método</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td><?php echo $order->get_order_number(); ?></td>
+								<td><?php echo date("d-m-Y", strtotime($order->get_date_created())); ?></td>
+								<td><?php echo $order->get_formatted_order_total(); ?></td>
+								<td>TARJETA</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
-				<div class="item content">
-					<ul>
-						<li><?php echo $order->get_order_number(); ?></li>
-						<li><?php echo date("d-m-Y", strtotime($order->get_date_created())); ?></li>
-						<li><?php echo $order->get_formatted_order_total(); ?></li>
-						<li>TARJETA</li>
-					</ul>
+
+				<div class="order">
+					<h2>DETALLA DE LA ORDEN</h2>
+					<table>
+						<thead>
+							<th>Producto</th>
+							<th>Cantidad</th>
+							<th>Total</th>
+						</thead>
+						<tbody>
+							<?php
+							foreach ($items as $item_id => $item) :
+								$item_total = $order->get_formatted_line_subtotal($item);
+							?>
+								<tr>
+									<td><?php echo $item->get_name(); ?></td>
+									<td><?php echo $item->get_quantity(); ?></td>
+									<td><?php echo $item_total;  ?></td>
+								</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
 				</div>
-			</main>
-		</div>
-		<div class="order">
-			<h2>DETALLE DE TU ORDEN</h2>
-			<main>
-				<div class="item header">
-					<ul>
-						<li>Producto</li>
-						<li>Cantidad</li>
-						<li>Precio</li>
-					</ul>
-				</div>
-				<?php
-				foreach ($items as $item_id => $item) :
-					$item_total = $order->get_formatted_line_subtotal($item);
-				?>
-					<div class="item content">
-						<ul>
-							<li><?php echo $item->get_name(); ?></li>
-							<li><?php echo $item->get_quantity(); ?></li>
-							<li><?php echo $item_total; ?></li>
-						</ul>
+			</div>
+		<?php else : ?>
+			<div class="mobile">
+				<div class="order">
+					<h2 class="title">TU ORDEN HA SIDO RECIBIDA</h2>
+					<div class="list">
+						<div class="list__item">
+							<div>
+								<strong>Número de orden</strong>
+								<p><?php echo $order->get_order_number(); ?></p>
+							</div>
+							<div>
+								<strong>Fecha</strong>
+								<p><?php echo date("d-m-Y", strtotime($order->get_date_created())); ?></p>
+							</div>
+						</div>
+						<div class="list__item">
+							<div>
+								<strong>Total</strong>
+								<p><?php echo $order->get_formatted_order_total(); ?></p>
+							</div>
+							<div>
+								<strong>Método</strong>
+								<p>TARJETA</p>
+							</div>
+						</div>
 					</div>
-				<?php endforeach; ?>
-			</main>
-		</div>
+				</div>
+				<div class="order">
+					<h2 class="title">DETALLE DE LA ORDEN</h2>
+					<div class="list">
+						<?php
+						$c = 0;
+						foreach ($items as $item_id => $item) :
+							$c++;
+							$item_total = $order->get_formatted_line_subtotal($item);
+						?>
+							<div class="list__item">
+								<div class="full">
+									<strong>Producto #<?php echo $c; ?></strong>
+									<p><?php echo $item->get_name(); ?></p>
+								</div>
+							</div>
+							<div class="list__item">
+								<div>
+									<strong>Cantidad</strong>
+									<p><?php echo $item->get_quantity(); ?></p>
+								</div>
+								<div>
+									<strong>Precio</strong>
+									<p><?php echo $item_total;  ?></p>
+								</div>
+							</div>
+						<?php
+						endforeach; ?>
+					</div>
+				</div>
+			</div>
+		<?php endif; ?>
 	</div>
 </section>
 

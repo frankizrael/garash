b647f8da09f88fb026462366631033bc17112151
Digital Cook <digitalcook@ronaldefg.xyz>
2020-06-22 00:36:57 +0000

    fix landing 1

--- wp-content/themes/garash/templates/template-landing2.php
+++ wp-content/themes/garash/templates/template-landing2.php
@@ -8,12 +8,14 @@
 set_query_var('ASSETS_KEY_WORD', 'page_home');
 get_header('header');
 ?>
+<link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+<script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
 <div class="lp_wrapper">
 	<div class="lp_header">
 		<div class="lp_container lp_flex">
 			<figure>
 				<a href="<?php echo site_url(); ?>">
-	            	<img src="<?php echo get_template_directory_uri() . '/retoz.png'  ?>" alt="Logo Garash">
+	            	<img src="<?php echo get_template_directory_uri() . '/assets/garage_logo_min.jpeg'  ?>" alt="Logo Garash">
 	            </a>
 	        </figure>
 	        <div class="information">
@@ -35,7 +37,7 @@ get_header('header');
                             <path d="m439.277344 72.722656c-46.898438-46.898437-109.238282-72.722656-175.566406-72.722656-.003907 0-.019532 0-.023438 0-32.804688.00390625-64.773438 6.355469-95.011719 18.882812-30.242187 12.527344-57.335937 30.640626-80.535156 53.839844-46.894531 46.894532-72.71875 109.246094-72.71875 175.566406 0 39.550782 9.542969 78.855469 27.625 113.875l-41.734375 119.226563c-2.941406 8.410156-.859375 17.550781 5.445312 23.851563 4.410157 4.414062 10.214844 6.757812 16.183594 6.757812 2.558594 0 5.144532-.429688 7.667969-1.3125l119.226563-41.730469c35.019531 18.082031 74.324218 27.625 113.875 27.625 66.320312 0 128.667968-25.828125 175.566406-72.722656 46.894531-46.894531 72.722656-109.246094 72.722656-175.566406 0-66.324219-25.824219-128.675781-72.722656-175.570313zm-21.234375 329.902344c-41.222657 41.226562-96.035157 63.925781-154.332031 63.925781-35.664063 0-71.09375-8.820312-102.460938-25.515625-5.6875-3.023437-12.410156-3.542968-18.445312-1.429687l-108.320313 37.910156 37.914063-108.320313c2.113281-6.042968 1.589843-12.765624-1.433594-18.449218-16.691406-31.359375-25.515625-66.789063-25.515625-102.457032 0-58.296874 22.703125-113.109374 63.925781-154.332031 41.21875-41.21875 96.023438-63.921875 154.316406-63.929687h.019532c58.300781 0 113.109374 22.703125 154.332031 63.929687 41.226562 41.222657 63.929687 96.03125 63.929687 154.332031 0 58.300782-22.703125 113.113282-63.929687 154.335938zm0 0"/><path d="m355.984375 270.46875c-11.421875-11.421875-30.007813-11.421875-41.429687 0l-12.492188 12.492188c-31.019531-16.902344-56.121094-42.003907-73.027344-73.023438l12.492188-12.492188c11.425781-11.421874 11.425781-30.007812 0-41.429687l-33.664063-33.664063c-11.421875-11.421874-30.007812-11.421874-41.429687 0l-26.929688 26.929688c-15.425781 15.425781-16.195312 41.945312-2.167968 74.675781 12.179687 28.417969 34.46875 59.652344 62.761718 87.945313 28.292969 28.292968 59.527344 50.582031 87.945313 62.761718 15.550781 6.664063 29.695312 9.988282 41.917969 9.988282 13.503906 0 24.660156-4.058594 32.757812-12.15625l26.929688-26.933594v.003906c5.535156-5.535156 8.582031-12.890625 8.582031-20.714844 0-7.828124-3.046875-15.183593-8.582031-20.714843zm-14.5 80.792969c-4.402344 4.402343-17.941406 5.945312-41.609375-4.195313-24.992188-10.710937-52.886719-30.742187-78.542969-56.398437s-45.683593-53.546875-56.394531-78.539063c-10.144531-23.667968-8.601562-37.210937-4.199219-41.613281l26.414063-26.414063 32.625 32.628907-15.636719 15.640625c-7.070313 7.070312-8.777344 17.792968-4.242187 26.683594 20.558593 40.3125 52.734374 72.488281 93.046874 93.046874 8.894532 4.535157 19.617188 2.832032 26.6875-4.242187l15.636719-15.636719 32.628907 32.628906zm0 0"/>
                         </svg>
                     </i>
-                    <p>Compra aquí</p>
+                    <p>Pide tu batería por Whatsapp</p>
                 </a>
 				<a href="https://www.facebook.com/messages/t/DIGCOOK" target="_blank">
                     <i>
@@ -46,10 +48,28 @@ get_header('header');
 			</div>
 		</div>
 	</div>
-	<section class="lp_home">
-		<video autoplay muted loop id="myVideo">
-		  <source src="<?php the_field('video'); ?>" type="video/mp4">
-		</video>
+	<section class="lp_home init">
+		
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <?php
+                    $banner = get_field('banner');
+                        if ($banner){
+                            foreach($banner as $ban) {
+                ?>
+                    <div class="swiper-slide">
+                        <div class="heightH initCore" style="background-image: url('<?php echo $ban['img']; ?>');">
+                        </div>
+                    </div>
+                <?php
+                        }
+                    }
+                ?>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+
+
 		<div class="lp_contact">
 			<div class="lp_contact_art"></div>
 			<div class="lp_form">
@@ -86,16 +106,25 @@ get_header('header');
 				</a>
 			</div>
 			<div class="lp_flex">
-				<?php
-					$bat = get_field('baterys_data');
-					foreach ($bat as $b) {
-						?>
-				<div class="lp_batery_item">
-					<img src="<?php echo $b['imagen']; ?>">
-				</div>
-						<?php
-					}
-				?>				
+                 <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <?php
+                            $bat = get_field('baterys_data');
+                                foreach ($bat as $b) {
+                        ?>
+                            <div class="swiper-slide">
+                                <div class="lp_batery_item">
+                                    <img src="<?php echo $b['imagen']; ?>">
+                                </div>
+                            </div>
+                        <?php
+                            }
+                        ?>
+                    </div>
+                </div>	
+
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>			
 			</div>
 		</div>
 		<div class="lp_banner lp_banner_2" style="background: url('<?php the_field('banner_imagen'); ?>');">
@@ -162,8 +191,31 @@ get_header('header');
 <?php
 get_template_part('partials/global/content', 'footer');
 ?>
-<style type="text/css">
+<script type="text/javascript">
+  var mySwiper = new Swiper ('.init .swiper-container', {
+    autoplay: 2500,
+    loop: true,
+    pagination: {
+      el: '.init .swiper-pagination',
+    }
+  });
 
+ var mySwiper = new Swiper ('.lp_flex .swiper-container', {
+    speed: 400,
+    navigation: {
+      nextEl: '.lp_flex .swiper-button-next',
+      prevEl: '.lp_flex .swiper-button-prev',
+    },
+    autoplay: 2500,
+    slidesPerView: 7
+  });
+
+      
+</script>
+<style type="text/css">
+.footer__content .list .group .item:nth-child(2) {
+    display: none;
+}
 #whatsapp {
     background: #01a827;
 }
@@ -192,7 +244,7 @@ get_template_part('partials/global/content', 'footer');
 .lp_fixed {
     position: fixed;
     top: 50%;
-    right: 0;
+    left: 0;
     transform: translateY(-50%);
 }
 .lp_header .information div p {
@@ -203,8 +255,8 @@ get_template_part('partials/global/content', 'footer');
 }
 .sharedModalItem {
     overflow: hidden;
-    border-top-left-radius: 7px;
-    border-bottom-left-radius: 7px;
+    border-top-right-radius: 7px;
+    border-bottom-right-radius: 7px;
 }
 .sharedModalItem a i {
     display: block;
@@ -253,7 +305,7 @@ section.lp_home video {
 .lp_contact {
     position: absolute;
     height: 100%;
-    left: 0;
+    right: 0;
     padding: 120px 0px;
     background: rgba(255, 255, 255, 0.92);
 }
@@ -357,7 +409,6 @@ section.lp_home video {
     font-size: 18px;
 }
 .lp_batery_item {
-    flex: 1;
     padding: 10px;
     box-shadow: 0px 8px 20px #00000054;
     margin: 10px;
@@ -366,13 +417,25 @@ section.lp_home video {
     border: solid 3px #424242;
     position: relative;
 }
+
 .lp_flex_l h3 {
     font-size: 25px;
 }
 .lp_banner.lp_banner_2 h2 {
     font-size: 35px;
 }
-
+.lp_batery .lp_flex .swiper-button-next {
+    right: -45px;
+}
+.lp_batery .lp_flex .swiper-button-prev {
+    left: -45px;
+}
+.lp_batery .lp_flex {
+    display: block;
+    max-width: 1600px;
+    margin: auto;
+    position: relative;
+}
 .lp_batery_item:before {
     content: '';
     width: 0;
@@ -500,7 +563,7 @@ section.lp_foot .lp_flex_l .text p {
     align-items: center;
 }
 .lp_header figure a img {
-    max-width: 70px;
+    max-width: 200px;
 }
 .lp_header figure a {
     display: block;
@@ -547,7 +610,7 @@ section.lp_foot .lp_flex_l .text p {
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
-    left: 450px;
+    left: 175px;
 }
 section.lp_home video {
     z-index: -1;
@@ -562,9 +625,25 @@ section.lp_home:before {
     height: 100%;
     background: black;
     top: 0;
-    z-index: 0;
+    z-index: 1;
     left: 0;
     opacity: 0.4;
+    pointer-events: none;
+}
+section.lp_home.init .swiper-container {
+    z-index: 0;
+}
+.initCore {
+    height: 100vh;
+    position: relative;
+    z-index: -1;
+}
+.lp_contact {
+    z-index: 5;
+    top: 0;
+}
+.get_titlte {
+    z-index: 3;
 }
 </style>
 <?php get_footer('footer'); ?>
